79d4ab797bb0d3e97ae16e9b5eaf621b59c0b44f
Matt Nicholson <mjn456@gmail.com>
2020-05-24 17:36:15 +0100

    credit on useintersect

--- src/components/Maquette/useIntersect.js
+++ src/components/Maquette/useIntersect.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-
+// https://medium.com/the-non-traditional-developer/how-to-use-an-intersectionobserver-in-a-react-hook-9fb061ac6cb5
 export default ({ root = null, rootMargin, threshold = 0 }) => {
   const [entry, updateEntry] = useState({});
   const [node, setNode] = useState(null);
